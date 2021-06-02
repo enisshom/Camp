14c735466a67ccceebd6061fc24cb34e67813bac
Enisshom <enisshom@gmail.com>
2021-06-02 11:52:16 +0100

    checkIn_numresa

--- resources/views/reservations/resa_attribute.blade.php
+++ resources/views/reservations/resa_attribute.blade.php
@@ -51,7 +51,7 @@
                     <div class="col-3">
                       @if($pax->nper==1)
                       
-                        <select class="form-control selectpicker roomSelect" title="Chambre" name="nchambre" id="select" roomId="{{$room->room_id}}" roomType="{{$resa->type}}">
+                        <select class="form-control  roomSelect" title="Chambre" name="nchambre" id="select" roomId="{{$room->room_id}}" roomType="{{$resa->type}}">
                           <option value="{{$room->number}}" selected>{{$room->number}}</option>
                           @if ($resa->freeRooms)
                             @foreach ($resa->freeRooms as $key=>$fr)
@@ -82,7 +82,8 @@
                       <input type="text" class="form-control" name="prenom"  aria-describedby="helpId" placeholder="Prénom">
                     </div>
                     <div class="col-3">
-                      <select class="form-control selectpicker" name="nationalit" >
+                      <select class="form-control " name="nationalit" >
+                        <option value="{{$pax->nationalit}}" >{{$pax->nationalit}}</option>
                         <option>Maroc</option>
                         <option>USA</option>
                         <option>Germany</option>
@@ -109,7 +110,7 @@
    
   <script>
     
-    $('select').selectpicker();
+    // $('select').selectpicker();
     /*Click on the room type*/
     // $(".type").on('click',function() {
 
@@ -151,17 +152,15 @@
             // $(this).show();
           }
         });
-        $(".roomSelect").each(function() {
-          // console.log($(this).val());
-            selected.forEach(sl => {
-            if ($(this).val() != sl) {
-                $("option[value='" + sl + "']", $(this)).hide();
+        $('.roomSelect').find('option').each(function(index,element){
+
+            if(selected.includes($(this).val())){
+              $(this).hide();
+            }else{
+              $(this).show();
             }
-            if(selected.includes("100"))
-            {
-              console.log('kayna')
-            }
-          });
+        
+
         });
         console.log(selected);
     });
